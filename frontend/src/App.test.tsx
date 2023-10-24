2b975137305abdae8c4ce7e49e0423646dbf832e
trungnguyenhuynhminh46 <trungnguyenhuynhminh46@gmail.com>
2023-10-24 20:51:18 +0700

    Fix vitest by adding run dev

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,13 +1,13 @@
-// import { render, screen } from "@testing-library/react";
-// import App from "./App";
-import {test} from "vitest";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import { test } from "vitest";
 
-// it("Should have hello world", ()=>{
-//     render(<App></App>);
-//     const message = screen.queryByText(/Hello from my app/i);
-//     expect(message).toBeVisible();
-// });
+it("Should have hello world", () => {
+  render(<App></App>);
+  const message = screen.queryByText(/Hello from my app/i);
+  expect(message).toBeVisible();
+});
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(3).toBe(3)
-})
+test("adds 1 + 2 to equal 3", () => {
+  expect(3).toBe(3);
+});
